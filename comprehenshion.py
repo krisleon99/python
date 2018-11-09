f8534c0128ffba71bd284acb92be1e228caff797
krisleon99 <chokokriss97@gmail.com>
2018-11-09 17:37:25 -0600

    List comprehension

---
+++ comprehenshion.py
@@ -0,0 +1,12 @@
+"""
+Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+"""
+
+__author__ = 'kris'
+
+
+if __name__ == '__main__':
+  a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+  comprehension = [num for num in a if num % 2 == 0]
+  print comprehension
+  
